250b4cf802bd6ec139653d9306e2412bd658afbe
greatif <greatif@live.de>
2018-09-16 02:41:14 +0200

    Create boot.php

---
+++ boot.php
@@ -0,0 +1,6 @@
+<?php
+
+rex_view::addJsFile( $this->getAssetsUrl('init-gallery.js') );
+rex_view::addJsFile( $this->getAssetsUrl('photoswipe-ui-default.min.js') );
+rex_view::addJsFile( $this->getAssetsUrl('photoswipe.min.js') );
+rex_view::addCssFile( $this->getAssetsUrl('photoswipe.css') );
