e8e094080dec91326c6134a3c016477a2e405e80
greatif <greatif@live.de>
2018-09-17 17:14:40 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -5,3 +5,4 @@ rex_view::addJsFile( $this->getAssetsUrl('photoswipe-ui-default.min.js') );
 rex_view::addJsFile( $this->getAssetsUrl('photoswipe.min.js') );
 rex_view::addCssFile( $this->getAssetsUrl('blog.css') );
 rex_view::addCssFile( $this->getAssetsUrl('photoswipe.css') );
+rex_view::addCssFile( $this->getAssetsUrl('default-skin.png') );
