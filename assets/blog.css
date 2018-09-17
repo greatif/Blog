fdc0b11450fea919d5cd202f9de289cdd42d90bf
greatif <greatif@live.de>
2018-09-17 16:25:00 +0200

    Create blog.css

---
+++ assets/blog.css
@@ -0,0 +1,152 @@
+.blog{
+overflow: hidden;
+}
+
+.blogbeitrag {
+	margin: 15px 0 15px 0;
+	padding-bottom: 15px;
+	border: 1px solid #808080;
+	border-radius: 0px;
+	overflow: hidden;
+	box-shadow: 5px 5px 10px rgba(150, 150, 150, 1);
+}
+
+.blogbeitrag p{
+	margin: 0px;
+	padding: 15px;
+}
+
+.blogbeitrag .head {
+	width: 100%;
+}
+
+.blogbeitrag .head h1 {
+	position: relative;
+	margin: 0px;
+	padding: 10px 0 10px 10px;
+	font-size: 15px;
+	line-height: 20px;
+	text-align: left;
+	color: #FFF;
+	text-shadow: 1px 1px 1px rgba(0, 0, 0, 1);
+	background-color: rgba(0, 0, 0, 0.7);
+	border-bottom: 1px solid #808080;
+	-webkit-box-shadow: 1px 1px 3px #969696;
+	-moz-box-shadow: 1px 1px 3px #969696;
+	box-shadow: 1px 1px 3px #969696;
+}
+
+.blogbeitrag .head img {
+	margin-top: -61px;
+	border-bottom: 1px solid #808080;
+	-webkit-box-shadow: 1px 1px 3px #969696;
+	-moz-box-shadow: 1px 1px 3px #969696;
+	box-shadow: 1px 1px 3px #969696;
+}
+
+.blogbeitrag .head:hover h1{
+	color: #FF6600;
+	background-color: rgba(255, 255, 255, 0.5);
+	-webkit-transition: all 1s ease;
+	-moz-transition: all 1s ease;
+	-o-transition: all 1s ease;
+	-ms-transition: all 1s ease;
+	transition: all 1s ease;
+}
+
+.blogbeitrag .teaser h1 {
+	font-size: 20px;
+	line-height: 30px;
+	text-align: left;
+}
+
+.blogbeitrag .teaser img {
+	margin-top: -81px;
+}
+
+.blogbeitrag .btn {
+    text-shadow: none;
+    font-weight: normal;
+	border-radius: 0px;
+	border-width: 1px;
+	box-shadow: 2px 2px 4px rgba(150, 150, 150, 1);
+}
+
+.blog {
+	width: 100%;
+}
+
+.blog .beitrag {
+	margin-bottom: 25px;
+}
+
+.blog .youtube iframe {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+.blog .youtube {
+	position: relative;
+	padding-bottom: 56.25%; /* Bestimmt das Seitenverhältnis */
+	padding-top: 0px;
+	height: 0;
+	overflow: hidden;
+}
+
+.blog .buttons {
+	margin: 0 0 25px 0;
+	text-align: center;
+}
+
+.blog-rss-abo {
+	width: 60px;
+	height: 55px;
+	z-index: 7000;
+	position: fixed;
+	background: rgba(255, 102, 0, 0.7);
+	border: 1px solid rgba(255, 255, 255, 1);
+	border-left: none;
+	-moz-border-radius-topright: 8px;
+	border-top-right-radius: 8px;
+	-moz-border-radius-bottomright: 8px;
+	border-bottom-right-radius: 8px;
+	bottom: 35%;
+	left: -45px;
+	padding: 5px 0 0 15px;
+	-webkit-box-shadow: 1px 1px 3px #969696;
+	-moz-box-shadow: 1px 1px 3px #969696;
+	box-shadow: 1px 1px 3px #969696;
+	-webkit-transition: all 1s ease;
+	-moz-transition: all 1s ease;
+	-o-transition: all 1s ease;%
+	-ms-transition: all 1s ease;
+	transition: all 1s ease;
+}
+
+.blog-rss-abo i {
+	color: #FFF;
+	font-size: 30px;
+}
+
+.blog-rss-abo:hover {
+	left: 0px;
+	background: rgba(255, 255, 255, 0.7);
+	border: 1px solid rgba(255, 102, 0, 1);
+	-webkit-transition: all 1s ease;
+	-moz-transition: all 1s ease;
+	-o-transition: all 1s ease;%
+	-ms-transition: all 1s ease;
+	transition: all 1s ease;
+}
+
+.blog-rss-abo:hover i {
+	color: #FF6600;
+	-webkit-transition: all 1s ease;
+	-moz-transition: all 1s ease;
+	-o-transition: all 1s ease;%
+	-ms-transition: all 1s ease;
+	transition: all 1s ease;
+}
