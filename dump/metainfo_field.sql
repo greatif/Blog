7b1bcfc9aad63a17fcdac362b28a191fb9579fcd
greatif <greatif@live.de>
2018-09-22 01:08:42 +0200

    Add files via upload

---
+++ dump/metainfo_field.sql
@@ -0,0 +1,3 @@
+INSERT INTO `rex_metainfo_field` (`title`, `name`, `priority`, `attributes`, `type_id`, `default`, `params`, `validate`, `callback`, `restrictions`, `createuser`, `createdate`, `updateuser`, `updatedate`)
+VALUES
+  ('Beschreibung','med_description',1,'',1,'','','','','','Administrator','2018-09-10 12:00:00','Administrator','2018-09-10 12:00:00');
---
+++ dump/yform_table_ycom_comment.sql
@@ -0,0 +1,3 @@
+INSERT INTO `rex_yform_table` (`status`, `table_name`, `name`, `description`, `list_amount`, `list_sortfield`, `list_sortorder`, `prio`, `search`, `hidden`, `export`, `import`, `mass_deletion`, `mass_edit`, `history`)
+VALUES
+  (1,'rex_ycom_comment','Community Kommentare','Ermöglicht Kommentare auf allen Artikeln, wenn man eingeloggt ist.',50,'id','DESC',3,1,0,0,0,1,0,0);
