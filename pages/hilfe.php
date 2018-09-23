fdd28241541925414f4e58dd9315e69ef0809b01
greatif <greatif@live.de>
2018-09-23 14:41:18 +0200

    Update hilfe.php

--- pages/hilfe.php
+++ pages/hilfe.php
@@ -1,9 +1,9 @@
 <?PHP
 
 if (rex_i18n::getLocale() != 'de_de') {
-    $file = rex_file::get(rex_path::addon('blog', 'README.md'));
+    $file = rex_file::get(rex_path::addon('gblog', 'README.md'));
 } else {
-    $file = rex_file::get(rex_path::addon('blog', 'README_de_de.md'));
+    $file = rex_file::get(rex_path::addon('gblog', 'README_de_de.md'));
 }
 
 $body = '<div class="markdown-body">' . rex_markdown::factory()->parse($file) . '</div>';
