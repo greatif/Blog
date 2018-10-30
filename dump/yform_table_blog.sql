968e4e3515f34c91e70d624fd3526ee4aa0b44f1
greatif <greatif@live.de>
2018-10-30 09:39:15 +0100

    Update yform_table_blog.sql

--- dump/yform_table_blog.sql
+++ dump/yform_table_blog.sql
@@ -1,3 +1,3 @@
 INSERT INTO `rex_yform_table` (`status`, `table_name`, `name`, `description`, `list_amount`, `list_sortfield`, `list_sortorder`, `prio`, `search`, `hidden`, `export`, `import`, `mass_deletion`, `mass_edit`, `history`)
 VALUES
-  (1,'rex_blog','Blog','Blog',50,'id','ASC',5,1,1,1,1,1,0,0);
+  (1,'rex_blog','Blog','Blog',50,'id','DESC',5,1,1,1,1,1,0,0);
